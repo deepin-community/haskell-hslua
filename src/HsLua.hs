f5ee3a2217476b0cf43312261ddcfc3dcd55069c
bluesky <chenchongbiao@deepin.org>
2023-10-10 14:03:05 +0800

    chore: updat repo
    update version to 2.1.0-1
    
    Issue: https://github.com/linuxdeepin/developer-center/issues/5731
    Log: update repo

---
+++ src/HsLua.hs
@@ -0,0 +1,48 @@
+{-|
+Module      : HsLua
+Copyright   : © 2007–2012 Gracjan Polak;
+              © 2012–2016 Ömer Sinan Ağacan;
+              © 2017-2022 Albert Krewinkel
+License     : MIT
+Maintainer  : Albert Krewinkel <tarleb+hslua@zeitkraut.de>
+Stability   : beta
+Portability : non-portable (depends on GHC)
+
+Functions and utilities enabling the seamless integration of a Lua
+interpreter into a Haskell project.
+
+This module combines and re-exports the functionality of the HsLua
+framework. Basic access to the Lua API is provided by @'Lua.Core'@ from
+Hackage package /lua/.
+-}
+module HsLua (
+    -- * Core functionality
+    module HsLua.Core
+    -- * Marshalling
+  , module HsLua.Marshalling
+    -- * Module, data, and function packaging
+  , module HsLua.ObjectOrientation
+  , module HsLua.Packaging
+    -- * Type classes
+  , module HsLua.Class.Exposable
+  , module HsLua.Class.Invokable
+  , module HsLua.Class.Peekable
+  , module HsLua.Class.Pushable
+    -- * Utility functions
+  , getglobal'
+  , setglobal'
+  , module HsLua.Class.Util
+  ) where
+
+import Prelude hiding (compare, concat)
+
+import HsLua.Core
+import HsLua.Class.Exposable
+import HsLua.Class.Invokable
+import HsLua.Class.Peekable
+import HsLua.Class.Pushable hiding (pushList)
+import HsLua.Class.Util
+import HsLua.Packaging
+import HsLua.Marshalling
+import HsLua.ObjectOrientation
+import HsLua.Util
--- test/test-hslua.hs
+++ test/test-hslua.hs
@@ -1,51 +1,22 @@
-{-
-Copyright © 2017-2019 Albert Krewinkel
+{-|
+Module      : Main
+Copyright   : © 2017-2022 Albert Krewinkel
+License     : MIT
+Maintainer  : Albert Krewinkel <tarleb+hslua@zeitkraut.de>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+Tests for HsLua.
 -}
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
-import qualified Foreign.LuaTests
-import qualified Foreign.Lua.CoreTests
-import qualified Foreign.Lua.FunctionCallingTests
-import qualified Foreign.Lua.ModuleTests
-import qualified Foreign.Lua.TypesTests
-import qualified Foreign.Lua.Types.PeekableTests
-import qualified Foreign.Lua.Types.PushableTests
-import qualified Foreign.Lua.UserdataTests
-import qualified Foreign.Lua.UtilTests
+import qualified HsLuaTests
+import qualified HsLua.UtilTests
 
 main :: IO ()
-main = defaultMain $ testGroup "hslua" tests
+main = defaultMain tests
 
 -- | HSpec tests
-tests :: [TestTree]
-tests =
-  [ Foreign.Lua.CoreTests.tests
-  , Foreign.Lua.FunctionCallingTests.tests
-  , Foreign.Lua.UtilTests.tests
-  , testGroup "Sendings and receiving values from the stack"
-    [ Foreign.Lua.TypesTests.tests
-    , Foreign.Lua.Types.PeekableTests.tests
-    , Foreign.Lua.Types.PushableTests.tests
-    ]
-  , Foreign.Lua.UserdataTests.tests
-  , Foreign.Lua.ModuleTests.tests
-  , Foreign.LuaTests.tests
+tests :: TestTree
+tests = testGroup "hslua"
+  [ HsLua.UtilTests.tests
+  , HsLuaTests.tests
   ]
